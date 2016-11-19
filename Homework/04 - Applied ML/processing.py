741121648cd00a8e696fe5e345e377c19cec1d94
hparmantier <hector.parmantier@epfl.ch>
2016-11-19 19:55:28 +0100

    Naive classifier Q1, done. In-the-middle, Feature Importance

--- Homework/04 - Applied ML/processing.py
+++ Homework/04 - Applied ML/processing.py
@@ -3,6 +3,7 @@ from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import cross_val_score
+import numpy as np
 
 
 def drop_columns(df, columns):
@@ -27,3 +28,35 @@ def label_encode(df, columns):
 
 def hot_encode(df, columns):
     return False
+
+def groups_to_lists(grouped, key):
+    #return grouped.aggregate(list)
+    return grouped.apply(lambda x: pd.Series(dict([[col,x[col].tolist()] for col in x if col not in [key]])))
+
+def has_same_value(col):
+    c = col.apply(lambda row: len(set(row)))
+    return all(row == 1 for row in c)
+
+def has_nan(col):
+    c = col.apply(lambda row: np.isnan(row).any())
+    return any(row == True for row in c)
+
+def replace_nan_in_list(list):
+    acc = []
+    last = np.nan
+    for l in list:
+        if not np.isnan(l):
+            last = l
+            break
+    if np.isnan(last):
+        return []
+    else:
+        for l in list:
+            if np.isnan(l):
+                acc.append(last)
+            else:
+                acc.append(l)
+    return acc
+
+def replace_nan(col):
+    return col.apply(replace_nan_in_list)
