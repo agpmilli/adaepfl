82cb67606dc72399f7919608ab36346013f9775f
hparmantier <hector.parmantier@epfl.ch>
2016-11-20 22:14:51 +0100

    renaming helper files.

---
+++ Homework/04 - Applied ML/classifiers_helper.py
@@ -0,0 +1,5 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn import preprocessing
+from sklearn.cross_validation import cross_val_score
+ 
