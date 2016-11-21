4d6ad729063d74783c5173aaf7392bdf835744c5
agpmilli <alain.milliet@epfl.ch>
2016-11-21 17:06:03 +0100

    1+bonus

--- Homework/04 - Applied ML/plot_learning_curve.py
+++ Homework/04 - Applied ML/plot_learning_curve.py
@@ -1,33 +1,16 @@
-"""
-========================
-Plotting Learning Curves
-========================
-
-On the left side the learning curve of a naive Bayes classifier is shown for
-the digits dataset. Note that the training score and the cross-validation score
-are both not very good at the end. However, the shape of the curve can be found
-in more complex datasets very often: the training score is very high at the
-beginning and decreases and the cross-validation score is very low at the
-beginning and increases. On the right side we see the learning curve of an SVM
-with RBF kernel. We can see clearly that the training score is still around
-the maximum and the validation score could be increased with more training
-samples.
-"""
-print(__doc__)
-
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import cross_validation
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.datasets import load_digits
-from sklearn.learning_curve import learning_curve
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
 
 
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
                         n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
     """
-    Generate a simple plot of the test and traning learning curve.
+    Generate a simple plot of the test and training learning curve.
 
     Parameters
     ----------
@@ -48,10 +31,20 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
     ylim : tuple, shape (ymin, ymax), optional
         Defines minimum and maximum yvalues plotted.
 
-    cv : integer, cross-validation generator, optional
-        If an integer is passed, it is the number of folds (defaults to 3).
-        Specific cross-validation objects can be passed, see
-        sklearn.cross_validation module for the list of possible objects
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - An object to be used as a cross-validation generator.
+          - An iterable yielding train/test splits.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
 
     n_jobs : integer, optional
         Number of jobs to run in parallel (default 1).
@@ -82,26 +75,3 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
 
     plt.legend(loc="best")
     return plt
-
-
-digits = load_digits()
-X, y = digits.data, digits.target
-
-
-title = "Learning Curves (Naive Bayes)"
-# Cross validation with 100 iterations to get smoother mean test and train
-# score curves, each time with 20% data randomly selected as a validation set.
-cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=100,
-                                   test_size=0.2, random_state=0)
-
-estimator = GaussianNB()
-plot_learning_curve(estimator, title, X, y, ylim=(0.7, 1.01), cv=cv, n_jobs=4)
-
-title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
-# SVC is more expensive so we do a lower number of CV iterations:
-cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=10,
-                                   test_size=0.2, random_state=0)
-estimator = SVC(gamma=0.001)
-plot_learning_curve(estimator, title, X, y, (0.7, 1.01), cv=cv, n_jobs=4)
-
-plt.show()
