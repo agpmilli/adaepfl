89b2957e429aabf3caa1d8a38badc65a6c17389d
alexcrt <alexis.cartier@gmail.com>
2016-11-24 15:45:51 +0100

    clean + confusion matrices for part1

--- Homework/04 - Applied ML/classifiers_helper.py
+++ Homework/04 - Applied ML/classifiers_helper.py
@@ -1,24 +1,3 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
-from sklearn import preprocessing
-from sklearn.cross_validation import cross_val_score
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-"""
-Plot the features' importance for a given classifier.
-
-@param - clf: the classifier
-@param - data_in: the input data 
-@param - labels_in: the output data
-@param - threshold_importance: the threshold required int the classification to plot it
-"""
-def plot_importances(clf, data_in, labels_in, treshold_importance):
-    values_out, labels_out = importances(clf, data_in, labels_in, treshold_importance)
-    graph = sns.barplot(labels_out, values_out, palette='GnBu_d')
-    graph.set_xticklabels(labels=labels_out, rotation=80)
-    plt.show()
-
 """
 Fit the data for the given classifier and return the features' importance with its labels
 
@@ -30,7 +9,6 @@ Fit the data for the given classifier and return the features' importance with i
 @return - the tuple (values_out, labels_out)
 """   
 def importances(clf, data_in, labels_in, treshold_importance):
-    #X_train, X_test, y_train, y_test = train_test_split(data_in, labels_in, test_size=0.20, random_state=0)
     importances = clf.fit(data_in, labels_in).feature_importances_
     l = [tup for tup in list(zip(importances, data_in.columns)) if tup[0] >= treshold_importance]
     l.sort(key=lambda tup: tup[0], reverse=True)
--- Homework/04 - Applied ML/preprocessing_helper.py
+++ Homework/04 - Applied ML/preprocessing_helper.py
@@ -1,16 +1,12 @@
-"""
-TODO REMOVE UNUSED IMPORT
-"""
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
-from sklearn.cross_validation import cross_val_score
-import numpy as np
 
 """
-encode the given columns using the preprocessing LabelEncoder object
-We fit the encoder on the possible values and then transform our  each column.
+Label encode each column in the dataframe given as parameter
+@param - df : the dataframe
+@param - columns: a list of columns to encode
+
+@return - a new dataframe with the given columns label encoded
 """
 def label_encode(df, columns):
     new = df.copy()
@@ -21,21 +17,16 @@ def label_encode(df, columns):
     return new
 
 """
-Encode the columns using the one-hot encode.
-returns a new dataframe with the new created columns.
+Hot encode each columns in the dataframe given as parameter
+@param - df : the dataframe
+@param - columns: a list of columns to encode
+
+@return - a new dataframe with the given columns hot encoded
 """
 def one_hot_encode(df, columns):
     new = df.copy()
-    ohe = preprocessing.OneHotEncoder()
     for col in columns:
         one_hot = pd.get_dummies(new[col])
         new = new.drop(col, axis=1)
         new = new.join(one_hot)
     return new
-
-
-"""
-aggregate a groupby dataframe object as an array per column.
-"""
-def groups_to_lists(grouped, key):
-    return grouped.apply(lambda x: pd.Series(dict([[col,x[col].tolist()] for col in x if col not in [key]])))
