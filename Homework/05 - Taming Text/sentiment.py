bd3f481c44e8f80dd070db31a1638c2481f18e32
hparmantier <hector.parmantier@epfl.ch>
2016-12-13 21:22:40 +0100

    vader sentiment analysis

--- Homework/05 - Taming Text/sentiment.py
+++ Homework/05 - Taming Text/sentiment.py
@@ -4,9 +4,17 @@ from nltk.corpus import twitter_samples, stopwords
 from nltk.sentiment.util import json2csv_preprocess
 from nltk.sentiment.util import parse_tweets_set
 from nltk.corpus import movie_reviews
+from sklearn.model_selection import train_test_split
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.svm import LinearSVC
+from nltk.sentiment.util import extract_unigram_feats
+from nltk.sentiment.util import extract_bigram_feats
+from nltk.classify import NaiveBayesClassifier
+from sklearn.model_selection import train_test_split
 
 
-######################### Tweeter demo #########################
+
+######################### Loaders #########################
 def load_twitter():
     tokenizer = TweetTokenizer(preserve_case=False)
     fields = ['id', 'text']
@@ -27,8 +35,50 @@ def load_twitter():
 def load_movies():
     pos_docs = [(list(movie_reviews.words(pos_id)), 'pos') for pos_id in movie_reviews.fileids('pos')]
     neg_docs = [(list(movie_reviews.words(neg_id)), 'neg') for neg_id in movie_reviews.fileids('neg')]
-    return pos_docs, neg_docs   
+    return pos_docs, neg_docs
 
 
-def train_classifier(classifier, pos_set, neg_set):
-    return 0
+##################### split ##########################
+
+def split_set(pos, neg):
+    train_pos, test_pos = train_test_split(pos, test_size=0.2)
+    train_neg, test_neg = train_test_split(neg, test_size=0.2)
+    return train_pos+train_neg, test_pos+test_neg
+
+def run_sa_twitt(train, test):
+    a = SentimentAnalyzer()
+    tr = NaiveBayesClassifier.train
+    all_words = [word for word in a.all_words(train)]
+    # Add simple unigram word features
+    unigram_feats = a.unigram_word_feats(all_words, top_n=1000)
+    a.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+
+    # Add bigram collocation features
+    bigram_collocs_feats = a.bigram_collocation_feats([tweet[0] for tweet in train_twitt],top_n=100, min_freq=12)
+    a.add_feat_extractor(extract_bigram_feats, bigrams=bigram_collocs_feats)
+
+    tr_set = a.apply_features(train)
+    test_set = a.apply_features(test)
+
+    #Training
+    clf = a.train(tr, tr_set)
+    res = a.evaluate(test_set)
+
+    print(res)
+
+def run_sa_mov(train, test):
+    a = SentimentAnalyzer()
+    tr = NaiveBayesClassifier.train
+    all_words = mov_analyzer.all_words(train)
+    # Add simple unigram word features
+    unigram_feats = a.unigram_word_feats(all_words, min_freq=4)
+    a.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+    # Apply features to obtain a feature-value representation of our datasets
+    tr_set = a.apply_features(train)
+    test_set = a.apply_features(test)
+
+    #Training
+    clf = a.train(tr, tr_set)
+    res = a.evaluate(test_set)
+
+    print(res)
